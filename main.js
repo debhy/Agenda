5def5747a07631aa01e5adde2f179b32b5b65573
debora <benaatydebora@gmail.com>
2023-05-21 10:00:48 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,30 @@
+var form = document.getElementById('formulario')
+var corpoAgenda = document.querySelector('.corpoTabela')
+var resetaAgenda = document.querySelector('.reset')
+var linhas = '';
+var nomes = []
+var numeros = []
+
+function adicionaLinha(){
+    var inputNome = document.querySelector('.Nome').value;
+    var inputNumero = parseInt(document.querySelector('.Telefone').value);
+
+    nomes.push(inputNome);
+    numeros.push(inputNumero);
+
+    var linha = '<tr class="itemNovo">';
+    linha += `<td>${inputNome}`;
+    linha += `<td>${inputNumero}`;
+
+    linhas += linha;
+}
+
+function atualizaTabela(){
+    corpoAgenda.innerHTML = linhas
+}
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    adicionaLinha();
+    atualizaTabela();
+})
